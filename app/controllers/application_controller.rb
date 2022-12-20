7c5b611a08b1482601a6aab5b6d064d542cd71e7
Joshua <joshsmayhew@gmail.com>
2022-12-20 15:07:15 +0700

    commit tree a2661d01d193928f1a1496bfc294ed765d311d52

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
 
@@ -9,11 +11,43 @@ class ApplicationController < Sinatra::Base
   get '/games/:id' do
     game = Game.find(params[:id])
 
-    game.to_json(only: [:id, :title, :genre, :price], include: {
-      reviews: { only: [:comment, :score], include: {
-        user: { only: [:name] }
-      } }
-    })
+    game.to_json(only: %i[id title genre price], include: {
+                   reviews: { only: %i[comment score], include: {
+                     user: { only: [:name] }
+                   } }
+                 })
   end
 
+  delete '/reviews/:id' do
+    # find the review using the ID
+
+    review_to_delete = Review.find(params[:id])
+    # delete the review
+
+    review_to_delete.destroy
+    # send a response with the deleted review as JSON
+
+    review_to_delete.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    review = Review.find(params[:id])\
+
+    review.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+
+    review.to_json
+  end
 end
